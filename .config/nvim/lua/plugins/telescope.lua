8b59c3c3c91759ff31cf77a1fca23d77d04cd263
skhoo <sam.khoo@freelancers.outfittery.de>
2024-05-07 07:53:32 +0200

    telescope show hidden files

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,23 @@
+local Util = require("lazyvim.util")
+
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- enable finding hidden/dot files
+    {
+      "<leader><leader>",
+      Util.telescope("files", { hidden = true, no_ignore = false }),
+      desc = "Find files (root dir)",
+    },
+    {
+      "<leader>ff",
+      Util.telescope("files", { hidden = true, no_ignore = false }),
+      desc = "Find files (root dir)",
+    },
+    {
+      "<leader>fF",
+      Util.telescope("files", { cwd = false, hidden = true, no_ignore = false }),
+      desc = "Find files (cwd)",
+    },
+  },
+}
