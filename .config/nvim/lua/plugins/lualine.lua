10d94ed5b696d2f743e10dd3b3e6910cc6c08a59
skhoo <sam.khoo@freelancers.outfittery.de>
2024-05-10 18:42:26 +0200

    lualine updates

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,49 +1,56 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.g.lualine_laststatus = vim.o.laststatus
-    if vim.fn.argc(-1) > 0 then
-      -- set an empty statusline till lualine loads
-      vim.o.statusline = " "
-    else
-      vim.o.laststatus = 3
-    end
-  end,
-  opts = function()
-    -- PERF: we don't need this lualine require madness 🤷
-    local lualine_require = require("lualine_require")
-    lualine_require.require = require
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.g.lualine_laststatus = vim.o.laststatus
+		if vim.fn.argc(-1) > 0 then
+			-- set an empty statusline till lualine loads
+			vim.o.statusline = " "
+		else
+			vim.o.laststatus = 3
+		end
+	end,
+	opts = function()
+		-- PERF: we don't need this lualine require madness 🤷
+		local lualine_require = require("lualine_require")
+		lualine_require.require = require
 
-    local icons = require("lazyvim.config").icons
+		local icons = require("lazyvim.config").icons
 
-    vim.o.laststatus = vim.g.lualine_laststatus
+		vim.o.laststatus = vim.g.lualine_laststatus
 
-    return {
-      options = {
-        theme = "auto",
-        globalstatus = true,
-        disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
+		return {
+			options = {
+				theme = "auto",
+				globalstatus = true,
+				disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+			},
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						fmt = function(str)
+							return " " .. str
+						end,
+					},
+				},
+				lualine_b = { "branch" },
 
-        lualine_c = {
-          LazyVim.lualine.root_dir(),
-          {
-            "diagnostics",
-            symbols = {
-              error = icons.diagnostics.Error,
-              warn = icons.diagnostics.Warn,
-              info = icons.diagnostics.Info,
-              hint = icons.diagnostics.Hint,
-            },
-          },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { LazyVim.lualine.pretty_path() },
-        },
-        lualine_x = {
+				lualine_c = {
+					LazyVim.lualine.root_dir(),
+					{
+						"diagnostics",
+						symbols = {
+							error = icons.diagnostics.Error,
+							warn = icons.diagnostics.Warn,
+							info = icons.diagnostics.Info,
+							hint = icons.diagnostics.Hint,
+						},
+					},
+					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					{ LazyVim.lualine.pretty_path() },
+				},
+				lualine_x = {
           -- stylua: ignore
           {
             function() return require("noice").api.status.command.get() end,
@@ -62,41 +69,41 @@ return {
             cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
             color = LazyVim.ui.fg("Debug"),
           },
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = LazyVim.ui.fg("Special"),
-          },
-          {
-            "diff",
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-            source = function()
-              local gitsigns = vim.b.gitsigns_status_dict
-              if gitsigns then
-                return {
-                  added = gitsigns.added,
-                  modified = gitsigns.changed,
-                  removed = gitsigns.removed,
-                }
-              end
-            end,
-          },
-        },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
-        },
-      },
-      extensions = { "neo-tree", "lazy" },
-    }
-  end,
+					{
+						require("lazy.status").updates,
+						cond = require("lazy.status").has_updates,
+						color = LazyVim.ui.fg("Special"),
+					},
+					{
+						"diff",
+						symbols = {
+							added = icons.git.added,
+							modified = icons.git.modified,
+							removed = icons.git.removed,
+						},
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
+				},
+				lualine_y = {
+					{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+					{ "location", padding = { left = 0, right = 1 } },
+				},
+				lualine_z = {
+					function()
+						return " " .. os.date("%R")
+					end,
+				},
+			},
+			extensions = { "neo-tree", "lazy" },
+		}
+	end,
 }
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# dotfiles
+# DOTFILES
 
 FONT
 ShureTechMono Nerd Font ( AKA ShareTech )
+
+
+## NOTES
+
+[GNU Stow](https://www.youtube.com/watch?v=y6XCebnB9gs) used to create a symlink between the git directory and $HOME for easier management
