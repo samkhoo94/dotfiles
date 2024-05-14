20235176f360281107569a005b4ae2e5cd635bb4
skhoo <sam.khoo@freelancers.outfittery.de>
2024-05-14 17:18:10 +0200

    vscode snippets

--- .config/nvim/lua/plugins/pylsp.lua
+++ .config/nvim/lua/plugins/pylsp.lua
@@ -1,22 +1,23 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pylsp = {
-          settings = {
-            pylsp = {
-              plugins = {
-                pycodestyle = { enabled = false },
-                flake8 = {
-                  enabled = true,
-                  maxLineLength = 120,
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            autoformat = false,
+            servers = {
+                pylsp = {
+                    settings = {
+                        pylsp = {
+                            plugins = {
+                                pycodestyle = { enabled = false },
+                                flake8 = {
+                                    enabled = true,
+                                    maxLineLength = 120,
+                                },
+                            },
+                        },
+                    },
                 },
-              },
             },
-          },
         },
-      },
     },
-  },
 }
