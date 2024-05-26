256645911a07d19740816cfeb9fc4e58f7288fca
skhoo <sam.khoo@freelancers.outfittery.de>
2024-05-26 11:43:35 +0800

    hardtime to improve vim habits

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -8,3 +8,5 @@ vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 vim.o.scrolloff = 10 -- Minimal amount of lines to keep above or below the cursor
+
+vim.o.showmode = false
---
+++ nvim/.config/nvim/lua/plugins/hardtime_premonition.lua
@@ -0,0 +1,14 @@
+return {
+    -- lazy.nvim
+    {
+        "m4xshen/hardtime.nvim",
+        event = "VeryLazy",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        opts = {},
+    },
+
+    {
+        "tris203/precognition.nvim",
+        config = {},
+    },
+}
