0875c013346169548f0995484ea572147668fb37
samkhoo94 <samkhoo77@gmail.com>
2024-06-04 07:03:09 +0800

    lspconfig changes

---
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -0,0 +1,3 @@
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
