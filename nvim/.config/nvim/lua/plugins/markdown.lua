d33d4a9e22b572d41e6c4f4b8920699e0611ebae
Sam Khoo <samkhoo77@gmail.com>
2025-07-11 12:23:13 +0100

    updates

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,17 +1,16 @@
 return {
-  -- add symbols-outline
-  {
-    -- "ixru/nvim-markdown",
-    -- cmd = "",
-    -- keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "[N]o [N]eckpain" } },
-  },
-
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
+    {
+        "iamcco/markdown-preview.nvim",
+        ft = { "markdown" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_auto_start = 0 -- don't auto-preview on open
+            vim.g.mkdp_auto_close = 1 -- auto-close when buffer closes
+            vim.g.mkdp_refresh_slow = 0
+            vim.g.mkdp_command_for_global = 0
+            vim.g.mkdp_open_to_the_world = 0
+            vim.g.mkdp_browser = "" -- use system default browser
+            vim.g.mkdp_echo_preview_url = 1 -- show URL so you can open it manually if needed
+        end,
+    },
 }
