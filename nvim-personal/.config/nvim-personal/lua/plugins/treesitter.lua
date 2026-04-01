d21a6499274b4d9bda9c32d8789c377f90e862e4
Sam Khoo <samkhoo77@gmail.com>
2026-04-01 21:13:13 +0100

    nvim personal - treesitter jinja

--- nvim-personal/.config/nvim-personal/lua/plugins/treesitter.lua
+++ nvim-personal/.config/nvim-personal/lua/plugins/treesitter.lua
@@ -1,16 +1,25 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "python", "lua", "go", "sql", "markdown", "markdown_inline",
-                "bash", "json", "yaml", "toml",
-            },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"python",
+				"lua",
+				"go",
+				"sql",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"json",
+				"yaml",
+				"toml",
+				"jinja",
+			},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
